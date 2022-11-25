7c8edd34aecc15a834dbb91cac3c5ae500e000b1
jaeyumn <91544507+jaeyumn@users.noreply.github.com>
2022-11-25 22:41:44 +0900

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"
+];
+
+const BG = "background"
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.className = BG;
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
